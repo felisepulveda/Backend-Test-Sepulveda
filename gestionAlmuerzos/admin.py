93a28098f8a4744c724e9c973debb78edf699b1c
PIPO <felandsepulveda@gmail.com>
2020-05-15 12:36:40 -0400

    Creacion de modelos

--- gestionAlmuerzos/admin.py
+++ gestionAlmuerzos/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from gestionAlmuerzos.models import Nora,Menu,Calendario,Empleado,Pedido
+
+admin.site.register(Nora)
+admin.site.register(Menu)
+admin.site.register(Calendario)
+admin.site.register(Empleado)
+admin.site.register(Pedido)
+
+
+
+
--- gestionAlmuerzos/models.py
+++ gestionAlmuerzos/models.py
@@ -2,4 +2,36 @@ from django.db import models
 
 # Create your models here.
 
+class Nora(models.Model): #id por defecto
+    nombre=models.CharField(blank=False,max_length=20,)
+    apellido=models.CharField(blank=False,max_length=20)
+    descripcion=models.TextField(blank=True)
+    email=models.EmailField(blank=True,null=True)
+    celular=models.IntegerField(blank=False)
+
+class Menu(models.Model): #id por defecto
+    plato_fuerte=models.CharField(blank=False,max_length=20)
+    ensalada=models.CharField(blank=True,max_length=20)
+    postre=models.CharField(blank=True,max_length=20)
+    nora=models.ForeignKey(Nora,on_delete=models.CASCADE, null=True)
+
+class Calendario(models.Model):
+    fecha=models.DateTimeField(auto_now_add=True,primary_key=True)
+    descripcion=models.TextField(blank=True)
+
+class Empleado(models.Model):
+    uuid=models.CharField(max_length=20,primary_key=True)
+
+class Pedido(models.Model):
+    empleado=models.ForeignKey(Empleado,on_delete=models.CASCADE)
+    calendario=models.ForeignKey(Calendario,on_delete=models.CASCADE)
+    customizacion=models.CharField(blank=True,max_length=20)
+    menu=models.ForeignKey(Menu,on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (('empleado', 'calendario'),)
+
+
+
+
 
